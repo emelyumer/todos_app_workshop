65da91db56d0d3ed8dab4ea20135be47271f6a60
emelyumer <emi_92rs@abv.bg>
2024-03-29 20:08:18 +0200

    set up

--- todos_app_workshop/todos/urls.py
+++ todos_app_workshop/todos/urls.py
@@ -1,3 +1,8 @@
+from django.urls import path
+
+from todos_app_workshop.todos.views import TodoListCreateApiView
+
 urlpatterns = [
+    path('', TodoListCreateApiView.as_view(), name='api_list_create_todos'),
 
 ]
